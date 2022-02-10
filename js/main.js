710f19c6ba1e98ec9f82e57999a6c6c0590a80c2
Chad Powell <chada.powell@outlook.com>
2022-02-09 21:41:45 -0500

    update main.js and style.css

--- js/main.js
+++ js/main.js
@@ -1,58 +1,83 @@
-/* VARIABLES CONNECTING THE JS FILE TO THE DOM ELEMENTS */
+/* VARIABLES CONNECTING TO THE DOM ELEMENTS */
 
 const customName = document.getElementById('customname');
-const randomize = document.getElementById('randomize');
-const story = document.getElementById('story');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
 
 /* FUNCTION TO GENERATE RANDOM VALUE FROM AN ARRAY */
 
-const getRandom = function (arr) {
-  const random = Math.floor(Math.random() * arr.length);
-  return arr[random];
+const randomValueFromArray = function (array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
 };
 
-/* VARIABLE HOLDING MAIN STORY TEXT, CONTAINING THREE INTERCHANGEABLE STRINGS STORED IN THREE SEPARATE ARRAYS */
-
-let storyText =
-  'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-
-let insertX = [
-  'Willy the Goblin',
-  'Big Daddy',
-  'Father Christmas',
-  'Mother Nature',
-  'Jack Frost',
-];
-
-let insertY = [
-  'the soup kitchen',
-  'Disneyland',
-  'the White House',
-  'Area 51',
-  'the Grand Canyon',
-];
-
-let insertZ = [
-  'spontaneously combusted',
-  'melted into a puddle on the sidewalk',
-  'turned into a slug and crawled away',
-  'smiled and quietly walked away',
-  'ran away screaming',
-];
-
-/* ADD EVENT LISTENER TO ACTIVATE A FUNCTION ON CLICK, CONNECTED TO THE RANDOMIZE DOM ELEMENT */
-
-randomize.addEventListener('click', result);
+/* FUNCTION TO GET RESULTS OF STORY GENERATOR */
 
 const result = function () {
-  /* IF NAME FIELD IS NOT LEFT BLANK THEN THE NAME ENTERED BY USER WILL BE DISPLAYED BY THIS FUNCTION */
+  /* VARIABLE FOR BASIC STORY TEXT */
 
-  if (customname.value !== '') {
+  let storyText = `It was 94 degrees fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`;
+
+  /* ARRAYS TO HOLD STORY VARIATIONS */
+
+  let insertX = [
+    'Willy the Goblin',
+    'Big Daddy',
+    'Father Christmas',
+    'Mother Nature',
+    'Jack Frost',
+  ];
+
+  let insertY = [
+    'the soup kitchen',
+    'Disneyland',
+    'the White House',
+    'Area 51',
+    'the Grand Canyon',
+  ];
+
+  let insertZ = [
+    'spontaneously combusted',
+    'melted into a puddle on the sidewalk',
+    'turned into a slug and crawled away',
+    'smiled and quietly walked away',
+    'ran away screaming',
+  ];
+
+  /* VARIABLES TO HOLD THE RANDOM STORY VARIATIONS AFTER PULLED BY randomValueFromArray() FUNCTION */
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  /* VARIABLE HOLDING UPDATED TEXT, AND REPLACING IT */
+  let newStory = storyText;
+  newStory = newStory.replaceAll(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
+  /* CONDITIONAL STATEMENT SETS NAME TO USER'S INPUT */
+  if (customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replace('Bob', name);
   }
 
+  /* IF UK CHECKED CONVERTS POUNDS TO STONES / FAHRENHEIT TO CELSIUS */
   if (document.getElementById('uk').checked) {
     const weight = Math.round(300 * 0.0714286);
-    const temperature = Math.round(((94 - 32) * 5) / 9);
+    const temperature = Math.round((94 - 32) / 1.8);
+
+    newStory = newStory.replace(`300 pounds`, `${weight} stones`);
+    newStory = newStory.replace(
+      `94 degrees fahrenheit`,
+      `${temperature} degrees Celsius`
+    );
   }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
 };
+
+/* EVENT LISTENER FOR RANDOMIZE BUTTON ELEMENT */
+
+randomize.addEventListener('click', result);
